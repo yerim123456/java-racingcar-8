02a37a4df06cac9a4668d5d709271e2de75cb455
yerim123456 <yearim1225@gmail.com>
2025-10-27 23:49:57 +0900

    feat: 프로그램 실행을 위한 진입점 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.domain.RandomMovePolicy;
+import racingcar.service.RacingService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingController controller = new RacingController(
+                new InputView(),
+                new OutputView(),
+                new RacingService(new RandomMovePolicy())
+        );
+        controller.run();
     }
 }
