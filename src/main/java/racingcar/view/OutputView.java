1f0ba7fae2a4f4fe7d4c99d69f7ce757952e3c52
yerim123456 <yearim1225@gmail.com>
2025-10-28 03:03:32 +0900

    fix(view): 객체 아닌 dto로 받아 출력하도록 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,16 +6,16 @@ import static racingcar.view.ViewMessages.OUTPUT_VIEW_ROUND_RESULT_MOVE_SYMBOL;
 import static racingcar.view.ViewMessages.OUTPUT_VIEW_ROUND_RESULT_TITLE;
 
 import java.util.List;
-import racingcar.domain.Cars;
+import racingcar.dto.CarsDto;
 
 public class OutputView {
     public void printRoundResultTitle() {
         System.out.println(OUTPUT_VIEW_ROUND_RESULT_TITLE.getMessage());
     }
 
-    public void printRoundResult(Cars cars) {
-        cars.getCars().forEach(car -> System.out.println(car.getName() + " : "
-                + OUTPUT_VIEW_ROUND_RESULT_MOVE_SYMBOL.getMessage().repeat(car.getLocation())));
+    public void printRoundResult(CarsDto cars) {
+        cars.carDtoList().forEach(car -> System.out.println(car.name() + " : "
+                + OUTPUT_VIEW_ROUND_RESULT_MOVE_SYMBOL.getMessage().repeat(car.location())));
         System.out.println();
     }
 
