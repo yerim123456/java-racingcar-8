1026c075c87472a2616df8da53e0a4f36220a0ef
yerim123456 <yearim1225@gmail.com>
2025-10-27 23:42:11 +0900

    feat(controller): RacingController 클래스 구현 및 게임 실행 흐름 관리

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,42 @@
+package racingcar.controller;
+
+import racingcar.domain.Cars;
+import racingcar.service.RacingService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingService racingService;
+
+    public RacingController(InputView inputView, OutputView outputView, RacingService racingService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.racingService = racingService;
+    }
+
+    public void run() {
+        // 입력 값 받아오기 (경주할 자동차 이름, 경주 차수)
+        String namesInput = inputView.readCarNames();
+        int roundCount = inputView.readRoundCount();
+
+        // 입력값 유효성 검증
+        RacingService.validateInputNamesIsNotNullAndEmpty(namesInput);
+        RacingService.validateInputRoundCountIsPlus(roundCount);
+
+        // 자동차 이름 값 검증 및 Cars 객체 생성
+        racingService.initCars(namesInput);
+
+        // 경주 차수에 따른 결과 출력
+        outputView.printRoundResultTitle();
+        for (int i = 0; i < roundCount; i++) {
+            Cars cars = racingService.playRound();
+            outputView.printRoundResult(cars);
+        }
+
+        // 경주 승자 출력
+        outputView.printWinners(racingService.getWinners());
+    }
+}
